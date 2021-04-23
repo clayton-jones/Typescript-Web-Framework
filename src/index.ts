f933e8301fd0b617fd3b9c098b1acccd26453309
Clayton Jones <claytonjonesLMP@gmail.com>
2021-04-22 21:02:49 -0700

    added set method to user class to update user data

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'me', age: 28 });
+const user = new User({});
+
+user.set({ name: 'NEW' });
 
 console.log(user.get('name'));
+console.log(user.get('age'));
